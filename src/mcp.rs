dff5118b141686bea4350db3103b90414256fbc6
PetoStr <petostrycek@centrum.sk>
2021-10-10 03:45:05 +0200

    feat: dummy file and process update

--- src/mcp.rs
+++ src/mcp.rs
@@ -25,6 +25,13 @@ const MEDUSA_COMM_KCLASSNAME_MAX: usize = 32 - 2;
 const MEDUSA_COMM_ATTRNAME_MAX: usize = 32 - 5;
 const MEDUSA_COMM_EVNAME_MAX: usize = 32 - 2;
 
+const MED_COMM_TYPE_END: u8 = 0x00;
+const _MED_COMM_TYPE_UNSIGNED: u8 = 0x01;
+const _MED_COMM_TYPE_SIGNED: u8 = 0x02;
+const _MED_COMM_TYPE_STRING: u8 = 0x03;
+const _MED_COMM_TYPE_BITMAP: u8 = 0x04;
+const _MED_COMM_TYPE_BYTES: u8 = 0x05;
+
 type Command = u32;
 
 lazy_static! {
@@ -43,39 +50,92 @@ lazy_static! {
 }
 
 // TODO refactor these weird names
-#[derive(Clone, Copy, Debug)]
-#[repr(packed)]
-pub struct MedusaCommKClass {
+#[derive(Default, Clone)]
+pub struct MedusaCommKClassHeader {
     kclassid: u64,
     size: i16,
     name: [u8; MEDUSA_COMM_KCLASSNAME_MAX],
 }
 
-impl MedusaCommKClass {
+impl MedusaCommKClassHeader {
     fn name(&self) -> String {
         cstr_to_string(&self.name)
     }
-
-    /*
-    fn to_le_bytes(&self) -> [u8; std::mem::size_of::<Self>()] {
-        // TODO prove safety
-        unsafe { std::mem::transmute(*self) }
-    }
-    */
 }
 
-#[derive(Clone, Copy)]
-#[repr(packed)]
-pub struct MedusaCommAttribute {
-    _offset: i16,
-    _length: i16,
-    _t: i8,
+#[derive(Default, Clone)]
+pub struct MedusaCommKClass {
+    header: MedusaCommKClassHeader,
+    attributes: Vec<MedusaCommAttribute>,
+}
+
+impl MedusaCommKClass {
+    fn set_attribute(&mut self, attr_name: &str, data: Vec<u8>) {
+        let name = self.header.name();
+        let mut attr = self
+            .attributes
+            .iter_mut()
+            .find(|x| x.header.name() == attr_name) // TODO HashMap, but preserve order like Vec does
+            .unwrap_or_else(|| panic!("{} has no attribute {}", name, attr_name));
+
+        attr.data = data;
+    }
+
+    fn _get_attribute(&mut self, attr_name: &str) -> &[u8] {
+        let name = self.header.name();
+        let attr = self
+            .attributes
+            .iter()
+            .find(|x| x.header.name() == attr_name) // TODO HashMap, but preserve order like Vec does
+            .unwrap_or_else(|| panic!("{} has no attribute {}", name, attr_name));
+
+        &attr.data
+    }
+
+    fn pack_attributes(&self) -> Vec<u8> {
+        let mut res = vec![0; self.header.size as usize];
+
+        for attribute in &self.attributes {
+            let data = attribute.pack_data();
+
+            // TODO make faster, `slice::copy_from_slice()` did not work
+            for i in 0..attribute.header.length as usize {
+                res[attribute.header.offset as usize + i] = data[i];
+            }
+        }
+
+        res
+    }
+}
+
+#[derive(Default, Clone)]
+pub struct MedusaCommAttributeHeader {
+    offset: i16,
+    length: i16, // size in bytes
+    r#type: u8, // i think this should be u8 and not i8 because of bit masks
     name: [u8; MEDUSA_COMM_ATTRNAME_MAX],
 }
 
-impl MedusaCommAttribute {
+impl MedusaCommAttributeHeader {
     fn name(&self) -> String {
         cstr_to_string(&self.name)
+    }
+}
+
+#[derive(Default, Clone)]
+pub struct MedusaCommAttribute {
+    header: MedusaCommAttributeHeader,
+    data: Vec<u8>,
+}
+
+impl MedusaCommAttribute {
+    fn pack_data(&self) -> Vec<u8> {
+        self.data
+            .iter()
+            .copied()
+            .chain(std::iter::once(0).cycle())
+            .take(self.header.length as usize)
+            .collect()
     }
 }
 
@@ -90,6 +150,7 @@ pub struct MedusaCommEvtype {
     ev_obj: u64,
     name: [u8; MEDUSA_COMM_EVNAME_MAX],
     ev_name: [[u8; MEDUSA_COMM_ATTRNAME_MAX]; 2],
+    // TODO attributes
 }
 
 impl MedusaCommEvtype {
@@ -162,7 +223,7 @@ impl DecisionAnswer {
 struct UpdateAnswer {
     kclassid: u64,
     msg_seq: u64,
-    ans_res: u32,
+    ans_res: i32,
 }
 
 #[derive(Clone, Debug)]
@@ -181,18 +242,24 @@ pub enum MedusaAnswer {
     Ok,
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone)]
 pub struct AuthRequestData {
     // TODO
     pub request_id: u64,
-    pub subject: MedusaCommKClass,
+    pub event: String,
+    pub subject: u64,
+    //pub object: MedusaCommKClass,
 }
 
+// TODO just use nom library
 trait Channel {
     fn read_u64(&mut self) -> io::Result<u64>;
     fn read_u32(&mut self) -> io::Result<u32>;
+    fn read_i16(&mut self) -> io::Result<i16>;
+    fn read_u8(&mut self) -> io::Result<u8>;
     fn read_kclass(&mut self) -> io::Result<MedusaCommKClass>;
     fn read_kevtype(&mut self) -> io::Result<MedusaCommEvtype>;
+    fn read_kattr_header(&mut self) -> io::Result<MedusaCommAttributeHeader>;
     fn read_kattrs(&mut self) -> io::Result<Vec<MedusaCommAttribute>>;
     fn read_command(&mut self) -> io::Result<Command>;
     fn read_update_answer(&mut self) -> io::Result<UpdateAnswer>;
@@ -242,6 +309,18 @@ impl<T: io::Read + io::Write> Channel for NativeEndianChannel<T> {
         Ok(u32::from_ne_bytes(buff))
     }
 
+    fn read_i16(&mut self) -> io::Result<i16> {
+        let mut buff = [0; 2];
+        self.handle.read_exact(&mut buff)?;
+        Ok(i16::from_ne_bytes(buff))
+    }
+
+    fn read_u8(&mut self) -> io::Result<u8> {
+        let mut buff = [0; 1];
+        self.handle.read_exact(&mut buff)?;
+        Ok(u8::from_ne_bytes(buff))
+    }
+
     fn read_command(&mut self) -> io::Result<Command> {
         let mut buff = [0; 4];
         self.handle.read_exact(&mut buff)?;
@@ -249,11 +328,20 @@ impl<T: io::Read + io::Write> Channel for NativeEndianChannel<T> {
     }
 
     fn read_kclass(&mut self) -> io::Result<MedusaCommKClass> {
-        let mut kclass_bytes = [0; std::mem::size_of::<MedusaCommKClass>()];
-        self.handle.read_exact(&mut kclass_bytes)?;
+        let kclassid = self.read_u64()?;
+        let size = self.read_i16()?;
 
-        // unsafe with mixed endianness between the server and the module
-        Ok(unsafe { std::mem::transmute(kclass_bytes) })
+        let mut name = [0; MEDUSA_COMM_KCLASSNAME_MAX];
+        self.handle.read_exact(&mut name)?;
+
+        Ok(MedusaCommKClass {
+            header: MedusaCommKClassHeader {
+                kclassid,
+                size,
+                name,
+            },
+            ..Default::default()
+        })
     }
 
     fn read_kevtype(&mut self) -> io::Result<MedusaCommEvtype> {
@@ -264,20 +352,36 @@ impl<T: io::Read + io::Write> Channel for NativeEndianChannel<T> {
         Ok(unsafe { std::mem::transmute(kevtype_bytes) })
     }
 
+    fn read_kattr_header(&mut self) -> io::Result<MedusaCommAttributeHeader> {
+        let offset = self.read_i16()?;
+        let length = self.read_i16()?;
+        let r#type = self.read_u8()?;
+
+        let mut name = [0; MEDUSA_COMM_ATTRNAME_MAX];
+        self.handle.read_exact(&mut name)?;
+
+        Ok(MedusaCommAttributeHeader {
+            offset,
+            length,
+            r#type,
+            name,
+        })
+    }
+
     fn read_kattrs(&mut self) -> io::Result<Vec<MedusaCommAttribute>> {
         let mut res = Vec::new();
 
         loop {
-            let mut kattr_bytes = [0; std::mem::size_of::<MedusaCommAttribute>()];
-            self.handle.read_exact(&mut kattr_bytes)?;
+            let header = self.read_kattr_header()?;
 
-            if kattr_bytes.iter().all(|&x| x == 0) {
+            if header.r#type == MED_COMM_TYPE_END {
                 break;
             }
 
-            // unsafe with mixed endianness between the server and the module
-            let kattr = unsafe { std::mem::transmute(kattr_bytes) };
-            res.push(kattr);
+            res.push(MedusaCommAttribute {
+                header,
+                ..Default::default()
+            });
         }
 
         Ok(res)
@@ -302,7 +406,7 @@ impl<T: io::Read + io::Write> Channel for NativeEndianChannel<T> {
             .get(&kclassid)
             .unwrap_or_else(|| panic!("Unknown class with id {:x}", kclassid));
 
-        let mut data = vec![0; class.size as usize];
+        let mut data = vec![0; class.header.size as usize];
         self.handle.read_exact(&mut data)?;
 
         Ok(FetchAnswer {
@@ -388,13 +492,25 @@ impl<T: Read + Write> Connection<T> {
                 let auth_data = self.acquire_auth_req_data(id)?;
                 let request_id = auth_data.request_id;
 
-                let result = auth_cb(auth_data) as u16;
+                if auth_data.event == "getfile" || auth_data.event == "getprocess" {
+                    let subject = self.classes.get_mut(&auth_data.subject).unwrap();
+                    if auth_data.event == "getfile" {
+                        //subject.set_attribute("med_oact", vec![0xff, 0xff]);
+                        subject.set_attribute("med_oact", vec![]);
+                        //subject.set_attribute("vs", vec![]);
+                    } else {
+                        //subject.set_attribute("med_oact", vec![0xff, 0xff]);
+                        //subject.set_attribute("med_sact", vec![0xff, 0xff]);
+                        subject.set_attribute("med_oact", vec![]);
+                        subject.set_attribute("med_sact", vec![]);
+                        //subject.set_attribute("vs", vec![]);
+                    }
 
-                //self.update_object(&auth_data.subject)?;
-                let printk = self.classes[&self.class_id["printk"]];
-                let mut msg = (0..50).map(|_| b'A').collect::<Vec<u8>>();
-                msg.push(0);
-                self.update_object(printk.kclassid, &msg)?;
+                    let packed_attrs = subject.pack_attributes();
+                    self.update_object(auth_data.subject, &packed_attrs)?;
+                }
+
+                let result = auth_cb(auth_data) as u16;
 
                 let decision = DecisionAnswer { request_id, result };
                 self.channel.write_all(&decision.as_bytes())?;
@@ -435,44 +551,58 @@ impl<T: Read + Write> Connection<T> {
         let ev_obj = acctype.ev_obj;
 
         // subject type
-        let sub_type = self.classes.get(&ev_sub).expect("Unknown subject type");
-        println!("sub_type name = {}", sub_type.name());
+        let sub_type = self.classes.get_mut(&ev_sub).expect("Unknown subject type");
+        println!("sub_type name = {}", sub_type.header.name());
 
-        let mut sub = vec![0; sub_type.size as usize];
+        // there seems to be padding so store into buffer first
+        let mut sub = vec![0; sub_type.header.size as usize];
         self.channel.read_exact(&mut sub)?;
-        println!("sub = {:?}", sub);
+
+        for attr in &mut sub_type.attributes {
+            let new_data =
+                sub[attr.header.offset as usize..][..attr.header.length as usize].to_vec();
+            attr.data = new_data;
+        }
 
         // object type
         if ev_obj != 0 {
             let obj_type = self.classes.get(&ev_obj).expect("Unknown object type");
-            println!("obj_type name = {}", obj_type.name());
+            println!("obj_type name = {}", obj_type.header.name());
 
-            let mut obj = vec![0; obj_type.size as usize];
+            let mut obj = vec![0; obj_type.header.size as usize];
             self.channel.read_exact(&mut obj)?;
             println!("obj = {:?}", obj);
         }
 
         Ok(AuthRequestData {
             request_id,
-            subject: *sub_type,
+            event: evtype.name(),
+            subject: ev_sub,
         })
     }
 
     fn register_kclass_def(&mut self) -> io::Result<()> {
-        let kclass = self.channel.read_kclass()?;
-        let size = kclass.size; // copy so there's no UB when referencing packed struct field
-        let name = kclass.name();
+        let mut kclass = self.channel.read_kclass()?;
+        let size = kclass.header.size; // copy so there's no UB when referencing packed struct field
+        let name = kclass.header.name();
         println!("kclass name = {}, size = {}", name, size);
 
         let kattrs = self.channel.read_kattrs()?;
-        print!("attributes:");
+        println!("attributes:");
         for attr in kattrs {
-            print!(" {}", attr.name());
+            println!(
+                "  attr={}, offset={}, type={:x}, len={}",
+                attr.header.name(),
+                attr.header.offset,
+                attr.header.r#type as u16,
+                attr.header.length
+            );
+            kclass.attributes.push(attr);
         }
         println!();
 
-        self.classes.insert(kclass.kclassid, kclass);
-        self.class_id.insert(name, kclass.kclassid);
+        self.class_id.insert(name, kclass.header.kclassid);
+        self.classes.insert(kclass.header.kclassid, kclass);
 
         Ok(())
     }
@@ -482,8 +612,6 @@ impl<T: Read + Write> Connection<T> {
         let ev_sub = kevtype.ev_sub;
         let ev_obj = kevtype.ev_obj;
 
-        //todo!("modify act bit..?");
-
         println!("kevtype name = {}", kevtype.name());
         println!("sub = 0x{:x}, obj = 0x{:x}", ev_sub, ev_obj);
 
@@ -491,8 +619,8 @@ impl<T: Read + Write> Connection<T> {
         let obj_type = self.classes.get(&ev_obj).expect("Unknown object type");
         println!(
             "sub name = {}, obj name = {}",
-            sub_type.name(),
-            obj_type.name()
+            sub_type.header.name(),
+            obj_type.header.name()
         );
         println!(
             "ev_name0 = {}, ev_name1 = {}",
@@ -509,7 +637,7 @@ impl<T: Read + Write> Connection<T> {
         let kattrs = self.channel.read_kattrs()?;
         print!("attributes:");
         for attr in kattrs {
-            print!(" {}", attr.name());
+            print!(" {}", attr.header.name());
         }
         println!();
 
@@ -530,7 +658,7 @@ impl<T: Read + Write> Connection<T> {
         println!("{:#?}", ans);
         println!(
             "class = {:?}",
-            self.classes.get(&{ ans.kclassid }).map(|c| c.name())
+            self.classes.get(&{ ans.kclassid }).map(|c| c.header.name())
         );
 
         Ok(())
@@ -538,7 +666,7 @@ impl<T: Read + Write> Connection<T> {
 
     fn fetch_answer(&mut self) -> io::Result<()> {
         let ans = self.channel.read_fetch_answer(&self.classes)?;
-        println!("{:#?}", ans);
+        println!("fetch_answer = {:#?}", ans);
 
         Ok(())
     }
